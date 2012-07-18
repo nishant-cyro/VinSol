a3a4caf43903189d579693610036af8d9ff7fdbc
Nishant <nishant@Nishant-PC.(none)>
2012-07-18 16:29:31 +0530

    jQuery1

--- jjs14.js
+++ jjs14.js
@@ -31,40 +31,57 @@ function removeCart(button) {
 }
 
 function addCart(button) {
-    var t = document.getElementById("table2");
+    var table = document.getElementById("table2");
     var flag = false;
-    for (var i = 0; i < t.rows.length; i = i + 1) {
-        if(t.rows[i].id === "rsec2"+button.id) {flag = true;} }
+    for (var i = 0; i < table.rows.length; i = i + 1) {
+        if(table.rows[i].id === "rsec2"+button.id) {flag = true;} }
     if(flag === true) {
-        var a = document.getElementById("quantitysec2" + button.id);
-        var b = document.getElementById("quantity" + button.id);
-        var c = document.getElementById("rsec2" + button.id);
-        document.getElementById("total").value = document.getElementById("total").value - c.cells[3].innerHTML;
-        a.value = eval(a.value + "+" + b.value);
-        c.cells[3].innerHTML = a.value * c.cells[1].innerHTML;
-        document.getElementById("total").value = eval((document.getElementById("total").value) + "+" + (c.cells[3].innerHTML));
+        var quantity2 = document.getElementById("quantitysec2" + button.id);
+        var quantity1 = document.getElementById("quantity" + button.id);
+        var rowsec2 = document.getElementById("rsec2" + button.id);
+        document.getElementById("total").value = document.getElementById("total").value - rowsec2.cells[3].innerHTML;
+        quantity2.value = eval(a.value + "+" + b.value);
+        quantity2.cells[3].innerHTML = quantity1.value * rowsec2.cells[1].innerHTML;
+        document.getElementById("total").value = eval((document.getElementById("total").value) + "+" + (rowsec2.cells[3].innerHTML));
     } else {
-        var a = document.getElementById("quantity" + button.id);
-        var b = document.getElementById("title" + button.id);
-        var c = document.getElementById("price" + button.id);
-        var d = document.getElementById("img" + button.id);
-        var r = t.insertRow(t.rows.length);
-        var cell1 = r.insertCell(0);
-        var cell2 = r.insertCell(1);
-        var cell3 = r.insertCell(2);
-        var cell4 = r.insertCell(3);
-        var cell5 = r.insertCell(4);
-        r.id = "rsec2" + button.id;
-        cell1.innerHTML = b.innerHTML + '<img src = "' + d.src + '" height="50" weight="50" />';
-        cell2.innerHTML = c.innerHTML;
-        cell2.id = "pricesec2" + button.id; 
-        cell3.innerHTML = '<input type = "text" id="quantitysec2' + button.id + '" size="3" value="' + a.value + '" onchange = "updateCart(this)" />';
-        cell4.innerHTML = c.innerHTML * a.value;
-        cell4.id = "subsec2" + button.id;
-        cell4.innerHTML = new Number(cell4.innerHTML).toFixed(2);
-        cell5.innerHTML = '<input type="button" id="' + button.id + '" value="Remove" onclick="removeCart(this)"/>';
-        document.getElementById("total").value = eval((document.getElementById("total").value) + "+" + (cell4.innerHTML));
-        document.getElementById("carts2").innerHTML = "My Carts(" + eval(t.rows.length + "- 1") +")";
+        var quantity = document.getElementById("quantity" + button.id);
+        var title = document.getElementById("title" + button.id);
+        var price = document.getElementById("price" + button.id);
+        var img = document.getElementById("img" + button.id);
+        var item_to_add = table.insertRow(table.rows.length);
+        var title2 = item_to_add.insertCell(0);
+        var unit_price2 = item_to_add.insertCell(1);
+        var quantity2 = item_to_add.insertCell(2);
+        var amount2 = item_to_add.insertCell(3);
+        var remove2 = item_to_add.insertCell(4);
+        item_to_add.id = "rsec2" + button.id;                 
+        title2.innerHTML = title.innerHTML; 
+
+        var img2 = document.createElement('img');     
+        img2.src = img.src;
+        img2.height = '50';
+        img2.width = '50';
+        title2.appendChild(img2);
+        unit_price2.innerHTML = price.innerHTML;
+        unit_price2.id = "pricesec2" + button.id; 
+        var quan2 = document.createElement("input");
+        quan2.type = 'text';
+        quan2.id = 'quantitysec2' + button.id;
+        quan2.size = '3';
+        quan2.value = quantity.value;
+        quan2.setAttribute('onchange', 'updateCart(this)');        
+        quantity2.appendChild(quan2);
+        amount2.innerHTML = price.innerHTML * quantity.value;
+        amount2.id = "subsec2" + button.id;
+        amount2.innerHTML = new Number(amount2.innerHTML).toFixed(2);
+        var remove = document.createElement('input');
+        remove.type = 'button';
+        remove.id = button.id;
+        remove.value = 'Remove';
+        remove.setAttribute('onclick','removeCart(this)');        
+        remove2.appendChild(remove);
+        document.getElementById("total").value = eval((document.getElementById("total").value) + "+" + (amount2.innerHTML));
+        document.getElementById("carts2").innerHTML = "My Carts(" + eval(table.rows.length + "- 1") +")";
     }
 }
 
--- jjs3.js
+++ jjs3.js
@@ -3,38 +3,87 @@ rno = 2;
 
 function toEdit(save)
 {
-  var row = document.getElementById("r"+save.name);
-  row.cells[0].innerHTML = '<input type = "text" id = "r' + save.name + 'c0" value ="' + row.cells[0].innerHTML + '" />';
-  row.cells[1].innerHTML = '<input type = "text" id = "r' + save.name + 'c1" value ="' + row.cells[1].innerHTML + '" />';
-  row.cells[2].innerHTML = '<input type = "button" id = "' + save.name + '" value = "Save" onclick = "insertRow(this)" />'; 
+    var row = document.getElementById("r"+save.name);
+    var name = document.createElement('input');
+    name.id = 'r' + save.name + 'c0';
+    name.type = 'text';
+    name.value = row.cells[0].innerHTML;
+    row.cells[0].innerHTML = '';
+    row.cells[0].appendChild(name);
+ 
+
+    var email = document.createElement('input');
+    email.id = 'r' + save.name + 'c1';
+    email.type = 'text';
+    email.value = row.cells[1].innerHTML;
+    row.cells[1].innerHTML = '';
+    row.cells[1].appendChild(email);
+  
+
+    var save1 = document.createElement('input');
+    save1.type = 'button';
+    save1.id = save.name;
+    save1.value = 'Save';
+    save1.setAttribute('onclick', 'changeRow(this)');
+    row.cells[2].innerHTML = '';
+    row.cells[2].appendChild(save1);
+
 }
 
 function toDelete(del)
 {
-  var element=document.getElementById("r"+del.name);
-  element.parentNode.removeChild(element);
+    var element=document.getElementById("r"+del.name);
+    element.parentNode.removeChild(element);
 }
 
 
 
-function changeRow(save)
-{
-  var row = document.getElementById("r"+save.id);
-  row.cells[0].innerHTML = document.getElementById("r"+save.id+"c0").value;
-  row.cells[1].innerHTML = document.getElementById("r"+save.id+"c1").value;
-  row.cells[2].innerHTML = '<a href="#edit" onclick="toEdit(this)" name="' + save.id + '" >Edit</a> / <a href = "#delete" name="' + save.id + '" onclick="toDelete(this)">Delete</a>';
+function changeRow(save) {
+    var row = document.getElementById("r"+save.id);
+    row.cells[0].innerHTML = document.getElementById("r"+save.id+"c0").value;
+    row.cells[1].innerHTML = document.getElementById("r"+save.id+"c1").value;
+    var edit = document.createElement('a');
+    edit.href = '#edit';
+    edit.innerHTML = 'Edit';
+    edit.name =save.id;
+    edit.setAttribute('onclick', 'toEdit(this)');
+    var del = document.createElement('a');
+    del.href = '#edit';
+    del.name =save.id;
+    del.innerHTML = 'Delete';
+    del.setAttribute('onclick', 'toDelete(this)');
+    row.cells[2].innerHTML = '';   
+    row.cells[2].appendChild(edit);
+    row.cells[2].appendChild(del);
 }
 
-function insertRow()
-{
-  var table=document.getElementById("myTable");
-  var row=table.insertRow(table.rows.length);
-  row.id = "r" + rno;
-  var cell1=row.insertCell(0);
-  var cell2=row.insertCell(1);
-  var cell3=row.insertCell(2);
-  cell1.innerHTML='<input type = "text" id = "r' + rno + 'c0" />';
-  cell2.innerHTML='<input type = "text" id = "r' + rno + 'c1" />';
-  cell3.innerHTML='<input type = "button" id = "' + rno + '" value = "Save" onclick = "changeRow(this)"/>';
-  rno = rno + 1;
+function insertRow() {
+
+    var table=document.getElementById('myTable');
+    var row=table.insertRow(table.rows.length);
+    row.id = "r" + rno;
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    var cell3=row.insertCell(2);
+    
+    var name = document.createElement('input');
+    name.id = 'r' + rno + 'c0';
+    name.type = 'text';
+    row.cells[0].appendChild(name);
+ 
+
+    var email = document.createElement('input');
+    email.id = 'r' + rno + 'c1';
+    email.type = 'text';
+    row.cells[1].appendChild(email);
+  
+
+    var save1 = document.createElement('input');
+    save1.type = 'button';
+    save1.id = rno;
+    save1.value = 'Save';
+    save1.setAttribute('onclick', 'changeRow(this)');
+    row.cells[2].appendChild(save1);
+
+     rno = rno + 1;
 }
